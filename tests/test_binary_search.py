0afd17ed6834bb81cc0a032e0059885fca61cd34
gukobrist <bogestvenniyiri@yandex.ru>
2021-02-24 19:42:09 +0300

    chsss

--- tests/test_binary_search.py
+++ tests/test_binary_search.py
@@ -1,29 +1,37 @@
 from ..binar_search import binar_search
 from pytest import raises
+from allure_commons._allure import step
 
 
 def test_search_element():
-    assert binar_search(2, [1, 2, 3, 4]) == 1
+    with step("-->Поиск элемента вконце"):
+        assert binar_search(2, [1, 2, 3, 4]) == 1
 
 
 def test_search_last_element():
-    assert binar_search(4, [1, 2, 3, 4]) == 3
+    with step("-->Поиск элемента в середине"):
+        assert binar_search(4, [1, 2, 3, 4]) == 3
 
 
 def test_search_first_element():
-    assert binar_search(1, [1, 2, 3, 4]) == 0
+    with step("-->Поиск элемента вначале"):
+        assert binar_search(1, [1, 2, 3, 4]) == 0
 
 
 def test_search_exception_not_found():
-    with raises(ValueError):
-        binar_search(-1, [2, 3, 4, 5])
+    with step("-->Проверяем исключение"):
+        with raises(ValueError):
+            binar_search(-1, [2, 3, 4, 5])
 
     with raises(ValueError):
-        binar_search(1, [2, 3, 4, 5])
+        with step("-->Проверяем исключение"):
+            binar_search(1, [2, 3, 4, 5])
 
     with raises(ValueError):
-        binar_search(5, [1, 2, 3, 4])
+        with step("-->Проверяем исключение"):
+            binar_search(5, [1, 2, 3, 4])
 
     with raises(ValueError):
-        binar_search(3, [1, 2, 4, 5])
+        with step("-->Проверяем исключение"):
+            binar_search(3, [1, 2, 4, 5])
 
--- tests/test_most_common.py
+++ tests/test_most_common.py
@@ -1,29 +1,34 @@
 from ..good_function_most_common import most_common_word_on_web_page
 from ..good_function_most_common import most_common_word
 from pytest import raises
+from allure_commons._allure import step
 
 
 def test_with_mock():
-    class TestResponse:
-        text = 'aa bbb c'
+    with step("-->Тест с моком"):
+        class TestResponse:
+            text = 'aa bbb c'
 
-    class TestUserAgent:
-        def get(self, url):
-            return TestResponse
+        class TestUserAgent:
+            def get(self, url):
+                return TestResponse
 
-    result = most_common_word_on_web_page(['a', 'b', 'c'], 'https://leadrock.com', user_agent=TestUserAgent())
+        result = most_common_word_on_web_page(['a', 'b', 'c'], 'https://leadrock.com', user_agent=TestUserAgent())
 
-    assert result == 'b', 'most common word'
+        assert result == 'b', 'most common word'
 
 
 def test_most_common_word():
-    assert most_common_word(['a', 'b', 'c'], 'abbbcc') == 'b', 'most common in one string'
+    with step("-->Тест с чистой функции"):
+        assert most_common_word(['a', 'b', 'c'], 'abbbcc') == 'b', 'most common in one string'
 
 
 def test_most_common_word_empty_candidate():
-    with raises(Exception, message='empty word raises'):
-        most_common_word([], 'abc')
+    with step("-->Тест с пустой строкой"):
+        with raises(Exception, message='empty word raises'):
+            most_common_word([], 'abc')
 
 
 def test_most_common_ambiguous_result():
-    assert most_common_word(['a', 'b', 'c'], 'ab') in ('a', 'b')
+    with step("-->Тест с непонятками"):
+        assert most_common_word(['a', 'b', 'c'], 'ab') in ('a', 'b')
